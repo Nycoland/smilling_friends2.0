953947610fc118b881075d26419efd6a97446c38
Nycoland <nycolepereira019@gmail.com>
2025-05-17 17:30:36 -0300

    editando controller

--- app/Http/Controllers/ClinicaController.php
+++ app/Http/Controllers/ClinicaController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Clinica;
 use Illuminate\Http\Request;
 
 class ClinicaController extends Controller
 {
-    //
+    public function index()
+    {
+        $clininas = Clinica::all();
+        return view('clinicas.index', compact('clinicas'));
+    }
+
+
+
+    public function create() 
+    {
+        return view('clinicas.create');
+    }
+
+
+
+    public function store(Request $request)
+    {
+
+    }
+
+
+
+
+    public function show(Clinica $clinica)
+    {
+        
+    }
+
+
+
+    public function edit(Clinica $clinica)
+    {
+
+    }
+
+
+    public function update(Request $request, Clinica $clinica)
+    {
+
+    }
+
+
+    public function destroy(Clinica $clinica)
+    {
+
+    }
 }
